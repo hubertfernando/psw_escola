bec64418700f3e5689a0effe35bd9287d9260bcf
hubert-fernando <hubertfernando97@gmail.com>
2025-07-22 16:10:50 -0300

    Criando as urls de disciplina

--- disciplina/urls.py
+++ disciplina/urls.py
@@ -5,7 +5,7 @@ from . import views # Importa as views do próprio app
 urlpatterns = [
     path('', views.index, name='index-disciplina'),
     path('<int:id_disciplina>/', views.detalha, name='index-detalha'),
-    path('cria/', views.create, name='create-disciplina'),
-    path('atualiza/<int:id_disciplina>', views.update, name='update-disciplina'),
+    path('cria/', views.cria, name='cria-disciplina'),
+    path('atualiza/<int:id_disciplina>', views.atualiza, name='atualiza-disciplina'),
     path('delete/<int:id_disciplina>', views.delete, name='delete-disciplina')
 ]
