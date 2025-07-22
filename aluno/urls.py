da2840f20bd4fb9b13985e914e00f04459b5770d
hubert-fernando <hubertfernando97@gmail.com>
2025-07-22 18:50:45 -0300

    Adicionando os templates

---
+++ aluno/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views # Importa as views do próprio app
+
+# Urls de aluno
+urlpatterns = [
+    path('', views.index, name='index-aluno'),
+    path('<int:id_aluno>/', views.detalha, name='index-detalha'),
+    path('cria/', views.cria, name='cria-aluno'),
+    path('atualiza/<int:id_aluno>', views.atualiza, name='atualiza-aluno'),
+    path('delete/<int:id_aluno>', views.delete, name='delete-aluno')
+]
--- escola/settings.py
+++ escola/settings.py
@@ -55,11 +55,11 @@ ROOT_URLCONF = 'escola.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # Esta linha foi adicionada/modificada para incluir a pasta de templates do projeto
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -67,7 +67,6 @@ TEMPLATES = [
         },
     },
 ]
-
 WSGI_APPLICATION = 'escola.wsgi.application'
 
 
