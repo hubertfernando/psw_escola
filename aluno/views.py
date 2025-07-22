d4a77356650d89bd08ca9ea1a72eb196ae0241cf
hubert-fernando <hubertfernando97@gmail.com>
2025-07-22 16:57:50 -0300

    Criando as views do app aluno

--- aluno/views.py
+++ aluno/views.py
@@ -1,3 +1,53 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Aluno
+from .forms import AlunoForm, AlunoEditForm
+from django.contrib.auth.decorators import login_required, permission_required
 
-# Create your views here.
+# View que lista todas os alunos cadastrados
+@login_required
+@permission_required('aluno.view_alunos', raise_exception=True)
+def index(request):
+    alunos = Aluno.objects.all()
+    return render(request, 'aluno/index.html', {'alunos': alunos})
+
+# View que mostra os detalhes de um aluno
+@login_required
+@permission_required('aluno.detail_alunos', raise_exception=True)
+def detalha(request, id_alunos):
+    aluno = Aluno.objects.get(id=id_alunos) 
+    return render(request, 'aluno/detalha.html', {'aluno': aluno})
+
+# View para criar aluno
+@login_required
+@permission_required('aluno.add_alunos', raise_exception=True)
+def cria(request):
+    if request.method == 'POST':
+        form = AlunoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/aluno/")    
+    else:
+        form = AlunoForm()
+    return render(request, 'aluno/cria.html', {'form': form})
+
+# View para editar alunos
+@login_required
+@permission_required('aluno.change_alunos', raise_exception=True)
+def atualiza(request, id_alunos):
+    aluno = Aluno.objects.get(id=id_alunos)
+    if request.method == 'POST':
+        form = AlunoEditForm(request.POST, instance=aluno)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/aluno/")    
+    else:
+        form = AlunoEditForm(instance=aluno)
+    return render(request, 'aluno/atualiza.html', {'form': form})
+
+# View para excluir um aluno
+@login_required
+@permission_required('aluno.delete_alunos', raise_exception=True)
+def delete(request, id_alunos):
+    Aluno.objects.get(id=id_alunos).delete()
+    return HttpResponseRedirect("/aluno/")
--- escola/urls.py
+++ escola/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include 
 
+from disciplina import views as views_disciplina
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('conta/', include('django.contrib.auth.urls')),
