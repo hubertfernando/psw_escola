1226a7be51fa29382472ae061646ab74199f34da
hubert-fernando <hubertfernando97@gmail.com>
2025-07-23 13:07:13 -0300

    Fazendo algumas correções

--- disciplina/urls.py
+++ disciplina/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.index, name='index-disciplina'),
     path('<int:id_disciplina>/', views.detalha, name='index-detalha'),
     path('cria/', views.cria, name='cria-disciplina'),
-    path('atualiza/<int:id_disciplina>', views.atualiza, name='atualiza-disciplina'),
-    path('delete/<int:id_disciplina>', views.delete, name='delete-disciplina')
+    path('atualiza/<int:id_disciplina>/', views.atualiza, name='atualiza-disciplina'),
+    path('delete/<int:id_disciplina>/', views.delete, name='delete-disciplina')
 ]
