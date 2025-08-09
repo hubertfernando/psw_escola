5b90d2ac196f851264fd48fe9400f3f214aa1c48
Juliana Pereira <julianapereirasantos1120@gmail.com>
2025-08-08 21:55:11 -0300

    Ajusta rotas do app turma

--- turma/urls.py
+++ turma/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
 
+app_name = 'turma'
+
 urlpatterns = [
-path('', views.index, name='index-turma'),
-path('<int:id_turma>/', views.detalhe, name='detalhe-turma'),
-path('add/', views.cria, name='add-turma'),
-path('edit/<int:id_turma>/', views.atualiza, name='edit-turma'),
-path('delete/<int:id_turma>/', views.deleta, name='delete-turma'),
+    path('', views.index, name='index-turma'),
+    path('<int:id_turma>/', views.detalhe, name='detalhe-turma'),
+    path('add/', views.cria, name='add-turma'),
+    path('edit/<int:id_turma>/', views.atualiza, name='edit-turma'),
+    path('delete/<int:id_turma>/', views.deleta, name='delete-turma'),
+    path('entrar/', views.entrar_por_codigo, name='entrar_turma'),  # rota para entrar via código
 ]
