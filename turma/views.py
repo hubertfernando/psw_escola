627c2b95232796436c153254a193d707902f84b9
Juliana Pereira <julianapereirasantos1120@gmail.com>
2025-08-08 21:55:27 -0300

    Melhora views do app turma

--- turma/views.py
+++ turma/views.py
@@ -1,47 +1,44 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from urllib.parse import urlparse, parse_qs
 from .models import Turma
 from .forms import TurmaForm
 
 
-# view que lista
 
+@login_required
 def index(request):
-    
-    turmas = Turma.objects.all()
-
+    turmas = Turma.objects.filter(membros=request.user)
     return render(request, 'turma/index.html', {'turmas': turmas})
 
-# view que detalha
 
+@login_required
 def detalhe(request, id_turma):
+    turma = get_object_or_404(Turma, id=id_turma)
+    link_convite_completo = request.build_absolute_uri(turma.get_link_convite())
+    return render(request, 'turma/detalhe.html', {
+        'turma': turma,
+        'link_convite_completo': link_convite_completo
+    })
 
-    turma = Turma.objects.get(id=id_turma)
-
-    return render(request, 'turma/detalhe.html', {'turma': turma})
-
-# view que cria
-
+@login_required
 def cria(request):
-
     if request.method == 'POST':
         form = TurmaForm(request.POST)
         if form.is_valid():
-            form.save()
+            turma = form.save(commit=False)
+            turma.lider = request.user
+            turma.save()
+            turma.membros.add(request.user)  # adiciona o criador como membro
             return HttpResponseRedirect("/turma/")
-
     else:
         form = TurmaForm()
-
     return render(request, 'turma/cria.html', {'form': form})
 
-# view que atualiza
-
+@login_required
 def atualiza(request, id_turma):
-    # recuperando a turma que será atualizada
-    turma = Turma.objects.get(pk=id_turma)
-
+    turma = get_object_or_404(Turma, pk=id_turma)
     if request.method == 'POST':
         form = TurmaForm(request.POST, instance=turma)
         if form.is_valid():
@@ -49,14 +46,35 @@ def atualiza(request, id_turma):
             return HttpResponseRedirect("/turma/")
     else:
         form = TurmaForm(instance=turma)
-
     return render(request, 'turma/atualiza.html', {'form': form})
 
-# view que apaga
-
+@login_required
 def deleta(request, id_turma):
-
-    turma = Turma.objects.get(id=id_turma)
+    turma = get_object_or_404(Turma, id=id_turma)
     turma.delete()
-
     return HttpResponseRedirect('/turma/')
+
+@login_required
+def entrar_por_codigo(request):
+    if request.method == 'POST':
+        codigo = request.POST.get('codigo_convite', '').strip()
+
+        # Extrai código do link completo, se for o caso
+        if codigo.startswith('http'):
+            from urllib.parse import urlparse, parse_qs
+            url_parts = urlparse(codigo)
+            query_params = parse_qs(url_parts.query)
+            codigo_lista = query_params.get('codigo')
+            if codigo_lista:
+                codigo = codigo_lista[0]
+            else:
+                return redirect('turma:index-turma')
+
+        turma = get_object_or_404(Turma, codigo_convite=codigo)
+        if request.user not in turma.membros.all():
+            turma.membros.add(request.user)
+
+        # Redireciona para o index (listagem), sem argumentos extras
+        return redirect('turma:index-turma')
+
+    return redirect('turma:index-turma')
